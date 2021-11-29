189502c8b1e944443cbd195fc67999abb5eb8207
SimonPGM <sigaleanom@unal.edu.co>
2021-11-28 21:43:34 -0500

    Partially finished

--- Appfolder/server.R
+++ Appfolder/server.R
@@ -1,3 +1,14 @@
 SERVER <- function(input, output) {
-  
+  output$plotVis <- renderPlot({
+    plot(1:5, main = input$plottypeVis)
+  })
+  output$plotPO <- renderPlot({
+    plot(1:5, main = input$acctypePO)
+  })
+  output$plotPT <- renderPlot({
+    plot(1:5, main = input$acctypePT)
+  })
+  output$plotPTh <- renderPlot({
+    plot(1:5, main = input$acctypePTh)
+  })
 }
--- Appfolder/ui.R
+++ Appfolder/ui.R
@@ -1,10 +1,154 @@
 Ui <- navbarPage("Análisis de accidentalidad en Medellín",
+                 includeCSS("./www/styles.css"),
                  tabPanel("Introducción"),
-                 tabPanel("Presentación de los datos"),
+                 tabPanel("Visualización de datos",
+                          fluidPage(
+                            fluidRow(
+                              column(10,
+                                     h4("Me la pelan putos"),
+                                     )
+                            ),
+                            fluidRow(
+                              column(2,
+                                     radioButtons("plottypeVis",
+                                                  "Tipo de visualización",
+                                                  c("Datos", "Mapa")),
+                                     dateInput("dateinputstartVis",
+                                               "Seleccione la fecha de inicio para la visualización",
+                                               value = "2014-07-04",
+                                               min = "2014-07-04",
+                                               max = "2020-08-31",
+                                               format = "dd-mm-yyyy",
+                                               language = "es"),
+                                     dateInput("dateinputendVis",
+                                               "Seleccione la fecha de fin para la visualización",
+                                               value = "2020-08-31",
+                                               min = "2014-07-04",
+                                               max = "2020-08-31",
+                                               format = "dd-mm-yyyy",
+                                               language = "es"),
+                                     selectInput("acctypeVis",
+                                                 "Elija el tipo de accidente para filtrar",
+                                                 c("Sin filtro", "Atropello", "Caída de ocupante",
+                                                   "Choque", "Volcamiento", "Incendio", "Otro"),
+                                                 selected = "Sin filtro"
+                                     )
+                                     ),
+                              column(10,
+                                     plotOutput("plotVis")
+                                     )
+                            )
+                          )
+                          ),
                  navbarMenu("Modelo predictivo",
-                            tabPanel("Periodo 2014-2018"),
-                            tabPanel("Periodo 2018-2019"),
-                            tabPanel("Periodo 2020-2021")
+                            tabPanel("Periodo 2014-2018",
+                                     fluidPage(
+                                       fluidRow(
+                                         column(10,
+                                                h4("Me la pelan putos"),
+                                         )
+                                       ),
+                                       fluidRow(
+                                         column(2,
+                                                dateInput("dateinputstartPO",
+                                                          "Seleccione la fecha de inicio para la visualización",
+                                                          value = "2014-07-04",
+                                                          min = "2014-07-04",
+                                                          max = "2017-12-31",
+                                                          format = "dd-mm-yyyy",
+                                                          language = "es"),
+                                                dateInput("dateinputendPO",
+                                                          "Seleccione la fecha de fin para la visualización",
+                                                          value = "2017-12-31",
+                                                          min = "2014-07-04",
+                                                          max = "2017-12-31",
+                                                          format = "dd-mm-yyyy",
+                                                          language = "es"),
+                                                selectInput("acctypePO",
+                                                            "Elija el tipo de accidente para filtrar",
+                                                            c("Sin filtro", "Atropello", "Caída de ocupante",
+                                                              "Choque", "Volcamiento", "Incendio", "Otro"),
+                                                            selected = "Sin filtro"
+                                                )
+                                         ),
+                                         column(10,
+                                                plotOutput("plotPO")
+                                         )
+                                       )
+                                     )
+                                     ),
+                            tabPanel("Periodo 2018-2019",
+                                     fluidPage(
+                                       fluidRow(
+                                         column(10,
+                                                h4("Me la pelan putos"),
+                                         )
+                                       ),
+                                       fluidRow(
+                                         column(2,
+                                                dateInput("dateinputstartPT",
+                                                          "Seleccione la fecha de inicio para la visualización",
+                                                          value = "2018-01-01",
+                                                          min = "2018-01-01",
+                                                          max = "2019-12-31",
+                                                          format = "dd-mm-yyyy",
+                                                          language = "es"),
+                                                dateInput("dateinputendPT",
+                                                          "Seleccione la fecha de fin para la visualización",
+                                                          value = "2019-12-31",
+                                                          min = "2018-01-01",
+                                                          max = "2019-12-31",
+                                                          format = "dd-mm-yyyy",
+                                                          language = "es"),
+                                                selectInput("acctypePT",
+                                                            "Elija el tipo de accidente para filtrar",
+                                                            c("Sin filtro", "Atropello", "Caída de ocupante",
+                                                              "Choque", "Volcamiento", "Incendio", "Otro"),
+                                                            selected = "Sin filtro"
+                                                )
+                                         ),
+                                         column(10,
+                                                plotOutput("plotPT")
+                                         )
+                                       )
+                                     )
+                                     ),
+                            tabPanel("Periodo 2020-2021",
+                                     fluidPage(
+                                       fluidRow(
+                                         column(10,
+                                                h4("Me la pelan putos"),
+                                         )
+                                       ),
+                                       fluidRow(
+                                         column(2,
+                                                dateInput("dateinputstartPTh",
+                                                          "Seleccione la fecha de inicio para la visualización",
+                                                          value = "2020-01-01",
+                                                          min = "2020-01-01",
+                                                          max = "2021-12-31",
+                                                          format = "dd-mm-yyyy",
+                                                          language = "es"),
+                                                dateInput("dateinputendPTh",
+                                                          "Seleccione la fecha de fin para la visualización",
+                                                          value = "2021-12-31",
+                                                          min = "2020-01-01",
+                                                          max = "2021-12-31",
+                                                          format = "dd-mm-yyyy",
+                                                          language = "es"),
+                                                selectInput("acctypePTh",
+                                                            "Elija el tipo de accidente para filtrar",
+                                                            c("Sin filtro", "Atropello", "Caída de ocupante",
+                                                              "Choque", "Volcamiento", "Incendio", "Otro"),
+                                                            selected = "Sin filtro"
+                                                )
+                                         ),
+                                         column(10,
+                                                plotOutput("plotPTh")
+                                         )
+                                       )
+                                     )
+                                     )
                             ),
                  tabPanel("Clasificación de los barrios"),
                  tabPanel("Información adicional")
