6b0d49c852af0b5988989b4be922173ab215abd2
SimonPGM <sigaleanom@unal.edu.co>
2021-11-30 19:47:22 -0500

    stop

--- Clustering/Clusters.R
+++ Clustering/Clusters.R
@@ -1,5 +1,6 @@
 library(tidyverse)
-datos <- readRDS("AccidentesMDE.Rds")
+library(magrittr)
+datos <- readRDS("datosClustering.Rds")
 datos <- datos %>%
   select(BARRIO, GRAVEDAD_ACCIDENTE, CLASE_ACCIDENTE) %>%
   group_by(BARRIO) %>%
@@ -8,3 +9,21 @@ datos <- datos %>%
             MAYOR_PROBABILIDAD = mean(CLASE_ACCIDENTE %in% c("Choque",
                                                              "Atropello"))) %>%
   ungroup()
+
+library(factoextra)
+
+datos[, 2] <- scale(datos[, 2])
+
+km1 <- kmeans(datos[,c(-1,-4)], centers = 4, nstart = 25)
+fviz_cluster(km1, data = datos[,c(-1,-4)])
+
+km2 <- kmeans(datos[,c(-1,-3)], centers = 4, nstart = 25)
+fviz_cluster(km2, data = datos[,c(-1, -3)])
+
+
+
+km3 <- kmeans(datos[,c(-1,-4)], centers = 3, nstart = 25)
+fviz_cluster(km3, data = datos[,c(-1,-4)])
+
+km4 <- kmeans(datos[,c(-1,-3)], centers = 3, nstart = 25)
+fviz_cluster(km3, data = datos[,c(-1,-3)])
